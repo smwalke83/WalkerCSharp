780dbaafaa8146a78b8c8fdf8286d04c311b027d
smwalke <smwalk83@gmail.com>
2026-05-25 11:29:51 -0600

    Ideal Gas Calculator Done

---
+++ StudentClassDemo/StudentClassDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using StudentClassDemo;
+
+namespace StudenClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string doAnother;
+            do
+            {
+                Student student = new Student();
+                string firstName = GetInput("First Name: ");
+                student.SetStudentFirstName(firstName);
+                string lastName = GetInput("Last Name: ");
+                student.SetStudentLastName(lastName);
+                string major = GetInput("Major: ");
+                student.SetMajor(major);
+                string studentNum = GetInput("Student Number: ");
+                student.SetStudentNumber(studentNum);
+                int score1 = int.Parse(GetInput("Score 1: "));
+                student.SetScore1(score1);
+                int score2 = int.Parse(GetInput("Score 2: "));
+                student.SetScore2(score2);
+                int score3 = int.Parse(GetInput("Score 3: "));
+                student.SetScore3(score3);
+
+                Console.WriteLine(student.GetSummary());
+
+                doAnother = GetInput("\nDo another? y/n: ");
+            } while (doAnother == "y");
+        }
+
+        private static string GetInput(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ StudentClassDemo/StudentClassDemo/Student.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentClassDemo
+{
+    class Student
+    {
+        // Fields
+        private string major;
+        private int score1;
+        private int score2;
+        private int score3;
+        private string studentFirstName;
+        private string studentLastName;
+        private string studentNumber;
+        private float average;
+
+        public Student()
+        {
+            // empty body
+        }
+
+        // Accessors (getters)
+        public string GetMajor()
+        {
+            return major;
+        }
+
+        public int GetScore1()
+        {
+            return score1;
+        }
+
+        public int GetScore2()
+        {
+            return score2;
+        }
+
+        public int GetScore3()
+        {
+            return score3;
+        }
+
+        public string GetStudentFirstName()
+        {
+            return studentFirstName;
+        }
+
+        public string GetStudentLastName()
+        {
+            return studentLastName;
+        }
+
+        public string GetStudentNumber()
+        {
+            return studentNumber;
+        }
+
+        // Read-only (getter, no setter)
+
+        public float GetAverage()
+        {
+            return average;
+        }
+
+        public string GetSummary()
+        {
+            Calc();
+            return studentFirstName + " " + studentLastName + " " + studentNumber + " Average: " + average;
+        }
+
+        // Mutators (setters)
+        public void SetMajor(string newMajor)
+        {
+            major = newMajor;
+        }
+
+        public void SetScore1(int newScore)
+        {
+            score1 = newScore;
+        }
+
+        public void SetScore2(int newScore)
+        {
+            score2 = newScore;
+        }
+
+        public void SetScore3(int newScore)
+        {
+            score3 = newScore;
+        }
+
+        public void SetStudentFirstName(string newName)
+        {
+            studentFirstName = newName;
+        }
+
+        public void SetStudentLastName(string newName)
+        {
+            studentLastName = newName;
+        }
+
+        public void SetStudentNumber(string newNumber)
+        {
+            studentNumber = newNumber;
+        }
+
+        // Private methods
+
+        private void Calc()
+        {
+            average = (score1 + score2 + score3) / 3;
+        }
+    }
+}
