d69668f5a2b7c5589bfc0b4b2039289f18c6f986
smwalke <smwalk83@gmail.com>
2026-06-01 12:56:18 -0600

    Complete Properties Demo

---
+++ PropertiesDemo/PropertiesDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿// Programmer: Scott Walker
+// Email: swalker62@student.cnm.edu
+// Project: Properties Demo
+// Purpose: Learning about properties
+
+using StudentClassDemo;
+
+namespace StudenClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initialize student list and do another variable
+            List<Student> students = new List<Student>();
+            string doAnother;
+            
+            // do/while loop to add students to list
+            do
+            {
+                Student st = new Student();
+                st.FirstName = GetInput("First Name");
+                st.LastName = GetInput("Last Name");
+                st.Major = GetInput("Major");
+                st.StudentNumber = GetInput("Student Number");
+                st.Score1 = int.Parse(GetInput("Score 1"));
+                st.Score2 = int.Parse(GetInput("Score 2"));
+                st.Score3 = int.Parse(GetInput("Score 3"));
+                students.Add(st);
+                doAnother = GetInput("\nDo another? y/n: ");
+            } while (doAnother == "y");
+
+            // Printout of student information for each student
+            Console.WriteLine("Student Averages: ");
+            foreach (Student st in students) 
+            {
+                Console.WriteLine(st.ToString());
+            }
+        }
+
+        // method to prompt a user for input
+        static private string GetInput(string prompt)
+        {
+            Console.Write(prompt + ": ");
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ PropertiesDemo/PropertiesDemo/Student.cs
@@ -0,0 +1,60 @@
+﻿// Programmer: Scott Walker
+// Email: swalker62@student.cnm.edu
+// Project: Properties Demo
+// Purpose: Learning about properties
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentClassDemo
+{
+    class Student
+    {
+        // Fields
+        // public static string[] majors = new string[] { "Programming", "Business", "Art History" }; This was used for the static variable demo, it will not be used in this demo
+        
+        // Auto implemented properties
+        public string Major { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StudentNumber { get; set; }
+        public double Average { get; private set; }
+
+        // Fully defined properties where setters also calculate average
+        private int score1;
+        public int Score1 
+        {
+            get { return score1; }
+            set { score1 = value; Calc(); }
+        }
+        private int score2;
+        public int Score2
+        {
+            get { return score2; }
+            set { score2 = value; Calc(); }
+        }
+        private int score3;
+        public int Score3 
+        {
+            get { return score3; }
+            set { score3 = value; Calc(); }
+        }
+
+        public Student()
+        {
+            // empty body - This was created in the initial demo, it should function the same as a default constructor
+        }
+
+        // ToString override method (previously GetSummary)
+        public override string ToString()
+        {
+            return "\nStudent Name: " + FirstName + " " + LastName + "\nStudent#: " + StudentNumber + "\nMajor: " + Major + "\nAverage: " + $"{Average:N2}";
+        }
+        // Calc method
+        private void Calc()
+        {
+            Average = (score1 + score2 + score3) / 3.0;
+        }
+    }
+}
