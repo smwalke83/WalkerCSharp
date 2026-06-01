45b5165b6a0cf8fa3d09a9874046bac0a1874cd9
smwalke <smwalk83@gmail.com>
2026-06-01 16:23:11 -0600

    Quiz 1 - Write a Class Completed

---
+++ WalkerQ1/WalkerQ1/ForceCalc.cs
@@ -0,0 +1,48 @@
+﻿// Programmer: Scott Walker
+// Email: swalker62@student.cnm.edu
+// File Name: WalkerQ1
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalkerQ1
+{
+    public class ForceCalc
+    {
+        // properties
+        private const double gConstant = 6.67408e-11;
+        public string Object1 { get; set; }
+        public string Object2 { get; set; }
+        public double GForce { get; private set; }
+        private double object1Mass;
+        public double Object1Mass 
+        {
+            get { return object1Mass; }
+            set { object1Mass = value; Calc(); }
+        }
+        private double object2Mass;
+        public double Object2Mass
+        {
+            get { return object2Mass; }
+            set { object2Mass = value; Calc(); }
+        }
+        private double radius;
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; Calc(); }
+        }
+
+        // methods - one to calc gravitational force and one to return a string to report the data to the user.
+        private void Calc() 
+        {
+            GForce = gConstant * ((object1Mass * object2Mass) / (Math.Pow(radius, 2)));
+        }
+
+        public override string ToString()
+        {
+            return "The gravitational force between " + Object1 + " and " + Object2 + " is " + $"{GForce:G4} m/s^2";
+        }
+    }
+}
---
+++ WalkerQ1/WalkerQ1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Programmer: Scott Walker
+// Email: swalker62@student.cnm.edu
+// File Name: WalkerQ1
+
+using System.Linq.Expressions;
+using WalkerQ1;
+
+string doAnother = "y";
+do
+{
+    
+    // Instantiating gforce object and getting user input values in a try/catch block. Exceptions are reported and the loop is restarted.
+    ForceCalc gForce = new ForceCalc();
+    try
+    {
+        gForce.Object1 = GetInput("Please enter the name of the first object: ");
+        gForce.Object1Mass = double.Parse(GetInput("Please enter the mass of " + gForce.Object1 + " in kilograms: "));
+        gForce.Object2 = GetInput("Please enter the name of the second object: ");
+        gForce.Object2Mass = double.Parse(GetInput("Please enter the mass of " + gForce.Object2 + " in kilograms: "));
+        gForce.Radius = (double.Parse(GetInput("Please enter the distance between the two objects in meters: "))) / 2;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Format error: Please enter a number.");
+        Console.WriteLine("Please try again.");
+        continue;
+    }
+    catch (Exception e) 
+    {
+        Console.WriteLine("Error: " + e);
+        Console.WriteLine("Please try again.");
+        continue;
+    }
+
+    // reporting results to the user and asking if they want to go again
+    Console.WriteLine(gForce.ToString());
+    Console.Write("Would you like to do another? y/n ");
+    doAnother = Console.ReadLine();
+} while (doAnother == "y");
+
+// method to get user input return the value
+static string GetInput(string prompt)
+{
+    Console.Write(prompt);
+    return Console.ReadLine();
+}
