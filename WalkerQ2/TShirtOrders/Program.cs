d059a0c9199f4a5f06508f9d7eae6ef5f4b2ed1b
smwalke <smwalk83@gmail.com>
2026-06-09 14:42:11 -0600

    Quiz 2 Debuggin Complete

---
+++ WalkerQ2/TShirtOrders/Program.cs
@@ -0,0 +1,122 @@
+﻿// Programmer: Scott Walker
+// Email: swalker62@student.cnm.edu
+// File: Program.cs
+// Project: Quiz 2
+
+using System;
+using System.Collections.Generic;
+namespace TShirtOrders
+{
+    class Program
+    {
+        private const string COMPANYNAME = "Super Shirt Ordermatic 3000";
+        private const string COMPANYSLOGAN = "The best shirt ordering system in the multiverse!";
+
+        static void Main(string[] args)
+        {
+            Header();
+            char option;
+            List<TShirtOrder> orders = new List<TShirtOrder>();
+            do
+            {
+                DisplayShirtOrders(orders);
+                option = GetStringFromUser("(A)dd shirt (R)emove shirt (T)otal (E)xit: ").Trim().ToUpper()[0];
+                switch(option)
+                {
+                    case 'A':
+                        AddShirtOrder(orders);
+                        break;
+                    case 'R':
+                        RemoveShirtOrder(orders);
+                        break;
+                    case 'T':
+                        DisplayTotal(orders);
+                        break;
+
+                }                
+            } while (option!='E');
+            Console.WriteLine("Thank you for using "+ COMPANYNAME);
+        }
+        private static void DisplayTotal(List<TShirtOrder> orders)
+        {
+            decimal total = 0;
+            foreach (TShirtOrder order in orders)
+            {
+                total += order.Price;
+            }
+            Console.WriteLine("Total price of order is: " + total);
+        }
+
+        private static void RemoveShirtOrder(List<TShirtOrder> orders)
+        {
+            int index = GetIntFromUser("Enter index of shirt order to remove: ");
+            if (GetBoolFromUser("Are you sure you want to delete this order"))
+            {
+                // This line was passing the index value into the Remove method, which wants the value of the item in the list, not the index. I also removed one from the user provided index as they are likely to provide a number starting from 1, not 0, based on the way the list is displayed to them
+                orders.Remove(orders[index -1]);
+            }
+        }
+        private static void AddShirtOrder(List<TShirtOrder> orders)
+        {
+            TShirtOrder order = new TShirtOrder();
+            order.FirstName = GetStringFromUser("Please enter your first name: ");
+            order.LastName = GetStringFromUser("Please enter your last name: ");
+            order.IsLocalPickup = GetBoolFromUser("Local pickup");
+            if (!order.IsLocalPickup)
+            {
+                order.Address = GetStringFromUser("Address: ");
+            }
+            order.OrderDate = DateTime.Now;
+            order.PrintAreaInSqIn = GetDoubleFromUser("Please enter area of your design in square inches: ");
+            order.NumColors = GetIntFromUser("Please enter number of colors: ");
+            order.NumShirts = GetIntFromUser("Please enter the number of shirts: ");
+            Console.WriteLine(order.ToString());
+            orders.Add(order);
+        }
+        private static void DisplayShirtOrders(List<TShirtOrder> orders)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Current shirts orders:");
+            if (orders.Count > 0)
+            {
+                // Orders.Count was initailly called like a method instead of a property ( orders.Count() )
+                for (int i = 0; i < orders.Count; ++i)
+                {
+                    Console.WriteLine((i + 1) + ": " + orders[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No shirts in order.");
+            }
+            Console.WriteLine();
+        }
+        private static void Header()
+        {
+            Console.WriteLine("Welcome to "+ COMPANYNAME+"!");
+            Console.WriteLine(COMPANYSLOGAN);
+            Console.WriteLine();
+        }
+        private static bool GetBoolFromUser(string prompt)
+        {
+            Console.Write(prompt + " (y/n)? ");
+            return Console.ReadLine().ToLower()[0] == 'y';
+        }
+        private static int GetIntFromUser(string prompt)
+        {
+            Console.Write(prompt);
+            return int.Parse(Console.ReadLine());
+        }
+        private static double GetDoubleFromUser(string prompt)
+        {
+            Console.Write(prompt);
+            return int.Parse(Console.ReadLine());
+        }
+
+        private static string GetStringFromUser(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ WalkerQ2/TShirtOrders/TShirtOrder.cs
@@ -0,0 +1,73 @@
+﻿// Programmer: Scott Walker
+// Email: swalker62@student.cnm.edu
+// File: TShirtOrder.cs
+// Project: Quiz 2
+
+using System;
+namespace TShirtOrders
+{
+    public class TShirtOrder
+    {
+        // The TShirtOrder constructor didn't provide a default value for "printAreaInSqIn". Non-optional parameters need to be declared first, however providing a default value seemed safer.
+        public TShirtOrder(string firstName="",string lastName = "", string address = "", DateTime? orderDate=null,bool isLocalPickup=true, double printAreaInSqIn=0.0, int numColors=1,int numShirts=1)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            OrderDate = orderDate;
+            IsLocalPickup = isLocalPickup;
+            this.printAreaInSqIn = printAreaInSqIn;
+            this.numColors = numColors;
+            this.numShirts = numShirts;
+            Calc();
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public bool IsLocalPickup { get; set; }
+        private double printAreaInSqIn;
+        public double PrintAreaInSqIn
+        {
+            get { return printAreaInSqIn; }
+            set { printAreaInSqIn = value; Calc(); }
+        }
+
+        private int numColors;
+        // The getter and setter for NumColors were returning and setting NumColors instead of the private numColors, leading to a recursive loop and stack overflow
+        public int NumColors
+        {
+            get { return numColors; }
+            set { numColors = value; Calc(); }
+        }
+
+        private int numShirts;
+        public int NumShirts
+        {
+            get { return numShirts; }
+            set { numShirts = value; Calc(); }
+        }
+        // The Price property initially had a private get, making it write only. The getter is now public. I set it to read-only (private get) as the calculations happen under the hood
+        public decimal Price { get; private set; }
+        private void Calc()
+        {
+            // Price is a decimal and the calculation was returning a double. It is now being cast into a decimal. There is technically a risk of the double being too large to cast to a decimal, so I added a Try/Catch block
+            try
+            {
+                Price = (decimal)(numShirts * (numColors * 2.25 + printAreaInSqIn * .05));
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+        public override string ToString()
+        {
+            // OrderDate.ToString was generating an error because the nullable DateTime class has no overloaded ToString methods. I saw a couple solutions online but the simplest seems to be adding a ? after OrderDate, making the ToString output nullable
+            return FirstName+" "
+                +LastName+" "
+                +OrderDate?.ToString("MM/dd/yyyy HH:mm:ss")+" "
+                +" Price: "+Price.ToString("c");
+        }
+    }
+}
