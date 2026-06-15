6505f1436cd75233a5e39d862818f0f6df636df4
smwalke <smwalk83@gmail.com>
2026-06-15 14:54:44 -0600

    WPF Demo Complete

---
+++ WPFDemo/WPFDemo/Assignment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFDemo
+{
+    public class Assignment
+    {
+        public string Title { get; set; }
+        private int score;
+
+        public int Score 
+        {
+            get { return score; }
+            set 
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    score = value;
+                }
+                else 
+                {
+                    throw new ArgumentOutOfRangeException("Scores must be between 0 and 100");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return Title + " " + score;
+        }
+    }
+}
---
+++ WPFDemo/WPFDemo/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Assignment> tempScores = new List<Assignment>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbScores.ItemsSource = tempScores;
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            Student st = new Student();
+
+            st.FirstName = txbFirstName.Text;
+            st.LastName = txbLastName.Text;
+            st.StudentNumber = txbStudentNumber.Text;
+            st.Major = txbMajor.Text;
+
+            st.Scores = tempScores;
+
+            txbResults.Text = st.ToString();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Assignment assign = new Assignment();
+            assign.Title = txbTitle.Text;
+            assign.Score = int.Parse(txbScore.Text);
+            tempScores.Add(assign);
+            lbScores.Items.Refresh();
+        }
+    }
+}
